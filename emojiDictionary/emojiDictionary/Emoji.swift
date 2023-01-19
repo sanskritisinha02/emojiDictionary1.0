778d8b0415fa6175ccd5a66fef9cd1c210df1450
Sanskriti Sinha <sanskritisinha.02@gmail.com>
2023-01-19 23:29:22 +0530

    Add files via upload

---
+++ emojiDictionary/emojiDictionary/Emoji.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji.swift
+//  emojiDictionary
+//
+//  Created by Sanskriti Sinha on 11/01/23.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+    
+}
+
---
+++ emojiDictionary/emojiDictionary/EmojiTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  EmojiTableViewCell.swift
+//  emojiDictionary
+//
+//  Created by Sanskriti Sinha on 14/01/23.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+    
+    
+    
+    @IBOutlet weak var symbolLabel: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    func update(with emoji: Emoji) {
+        symbolLabel.text = emoji.symbol
+        nameLabel.text = emoji.name
+        descriptionLabel.text = emoji.description
+    }
+    
+    
+    
+    
+    
+    
+}
